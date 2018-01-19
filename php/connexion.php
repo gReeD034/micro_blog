c1c14e66d935b4b2c4ef36a5ce0bb8ff5377938d
Soltysiak <s.isoltysiak@gmail.com>
2018-01-19 18:47:29 +0100

    bdd

--- php/connexion.php
+++ php/connexion.php
@@ -1,3 +1,8 @@
-<?php $pdo = new PDO('mysql:host=localhost;dbname=micro_blog', 'root', 'root');
+<?php
+try{
+	$pdo = new PDO('mysql:host=localhost;dbname=micro_blog_soltysiak_samuel', 'root', '');
+}catch (PDOException $e){
+	echo 'Connexion échouée : ' . $e->getMessage();
+}
 
 ?>
