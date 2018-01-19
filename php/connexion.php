7027d1d134a48cf0e983b3ff6fc8eeb30dbed897
Soltysiak <s.isoltysiak@gmail.com>
2018-01-19 18:16:10 +0100

    corrections mineurs

--- php/connexion.php
+++ php/connexion.php
@@ -1,12 +1,3 @@
-<?php $pdo = new PDO('mysql:host=localhost;dbname=micro_blog', 'root', 'root'); 
-	/*if (isset ($_POST['email'])){
-		1. util.ok?
-		2. $sid=mds($_POST['email'])+time();
-		3. MAJ BDD
-		4. Création cookie
-	}
-	else{
-		<form action="connexion.php"
-	}
-	/*
+<?php $pdo = new PDO('mysql:host=localhost;dbname=micro_blog', 'root', 'root');
+
 ?>
